7d4b4a97df3658db213d40fb9e099e6077653fd4
wawawa113 <ryo241334@icloud.com>
2021-07-27 17:05:09 +0900

    created main file

--- hoge.cpp
+++
@@ -1,6 +0,0 @@
-#include "fileRead.cpp"
-
-int main(){
-    text t=readFile("test.txt");
-    for(int i=0;i<t.size();i++)cout<<t[i]<<endl;
-}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "changer1.cpp"
+#include "fileRead.cpp"
+
+int main()
+{
+    text t = readFile("test.txt");
+    text result = changeWord(t);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+}
